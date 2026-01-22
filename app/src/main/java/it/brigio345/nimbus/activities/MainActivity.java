3da097274eaaac530f4c9a2d32b0fbbd763d905c
Giovanni Brignone <brigio345@gmail.com>
2026-01-23 00:07:22 +0100

    activities: MainActivity: fix the pager adapter titles
    Pager adapter titles were not shown.
    
    Signed-off-by: Giovanni Brignone <brigio345@gmail.com>

--- app/src/main/java/it/brigio345/nimbus/activities/MainActivity.java
+++ app/src/main/java/it/brigio345/nimbus/activities/MainActivity.java
@@ -10,6 +10,7 @@ import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Build;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -190,6 +191,9 @@ public class MainActivity extends AppCompatActivity {
         pagerAdapter = new MainPagerAdapter(getSupportFragmentManager());
         viewPager = findViewById(R.id.viewpager_main);
         viewPager.setAdapter(pagerAdapter);
+
+        TabLayout tabLayout = findViewById(R.id.tablayout_main);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     @Override
